0e59ccaff396c481303a0041889133634bd188c8
Thiago Arrais <thiago.arrais@gmail.com>
2013-07-04 19:51:24 -0300

    Make cm the default unit

--- app.rb
+++ app.rb
@@ -7,16 +7,18 @@ require 'print_chart/display'
 require 'print_chart/resolution_reference'
 require 'print_chart/size_reference'
 
-get '/:width;:height/:unit' do #TODO make cm default unit
-  content_type :svg
-  w, h = params[:width].to_i, params[:height].to_i
-  #TODO respond with 404 if w and h aren't numbers
-  unit = PrintChart::UNITS[params[:unit]]
-  #TODO respond with 404 if unit not found
-  refs = PrintChart::ResolutionReference.new(unit.resolutions)
-  chart = PrintChart::SizeReference.new(unit.sizes).chart_for(w, h, refs)
-  @largest_good = chart.largest_good
-  @largest_great = chart.largest_great
-  @render = PrintChart::Display.new(refs)
-  erb :'ruler.svg'
+['/:width;:height/:unit', '/:width;:height'].each do |route|
+  get route do
+    content_type :svg
+    w, h = params[:width].to_i, params[:height].to_i
+    #TODO respond with 404 if w and h aren't numbers
+    unit = PrintChart::UNITS[params[:unit] || 'cm']
+    #TODO respond with 404 if unit not found
+    refs = PrintChart::ResolutionReference.new(unit.resolutions)
+    chart = PrintChart::SizeReference.new(unit.sizes).chart_for(w, h, refs)
+    @largest_good = chart.largest_good
+    @largest_great = chart.largest_great
+    @render = PrintChart::Display.new(refs)
+    erb :'ruler.svg'
+  end
 end
